4511b6cba411d29482de814ed6757e95bcad66e1
PRANEET <127319827+Nani0778@users.noreply.github.com>
2024-08-27 00:49:08 +0530

    Update Github.py

--- plugins/Extra/Github.py
+++ plugins/Extra/Github.py
@@ -8,13 +8,13 @@ from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client, filters
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('✨ Made By ✨', url='https://t.me/movieplaza77')]])
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('✨ Made By ✨', url='https://t.me/ramsssfiles')]])
 A = """{} with user id:- {} used /git command."""
 
 @Client.on_message(filters.command(["github", "git"]))
 async def getgithub(bot, message):
     if len(message.command) != 2:
-        await message.reply_text("/github Username \n\n Like:- `/github movieplaza77`", quote=True)
+        await message.reply_text("/github Username \n\n Like:- `/github ramsssfiles`", quote=True)
         return
     await message.reply_chat_action(enums.ChatAction.TYPING)
     k = await message.reply_text("**Processing...⏳**", quote=True)    
